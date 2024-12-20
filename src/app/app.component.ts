66cb70a79ffafaa147e42ea181c1bd15135bfb29
JesseKemppi <jesse.kemppi@student.lab.fi>
2024-12-20 13:26:53 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from "@angular/material/icon";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-example-app';
+  toolbarText = 'Dev-ops-kurssin harjoitusprojekti';
+}
